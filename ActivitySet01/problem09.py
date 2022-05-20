b8b7c773b2492265e99af952e6847817199f8635
guna sekhar <93110449+Gunasekhar2002@users.noreply.github.com>
2022-05-20 04:07:28 +0000

    p9

--- ActivitySet01/problem09.py
+++ ActivitySet01/problem09.py
@@ -1,3 +1,14 @@
 # Lists
 
-filename = "dataset/romeo.txt"
+
+fname = input("Enter file name:")
+fh = open(fname)
+lst=[]
+for line in fh:
+    L=line.split()
+    for i in L:
+        if i not in lst:
+            lst.append(i)
+            
+lst.sort()
+print(lst)
