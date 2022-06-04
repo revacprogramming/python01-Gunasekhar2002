8307d3593f3302e7aba545f4916577594101f193
guna sekhar <93110449+Gunasekhar2002@users.noreply.github.com>
2022-06-04 15:28:01 +0000

    p11 A1

--- ActivitySet01/problem11.py
+++ ActivitySet01/problem11.py
@@ -1,3 +1,21 @@
 # Tuples
 
-filename = "dataset/mbox-short.txt"
+name = input("Enter file:")
+if len(name) < 1:
+    name = "mbox-short.txt"
+handle = open(name)
+hcount={}
+hlist=[]
+for i in handle:
+    wd=i.split()
+    if len(wd)>1 and wd[0]=='From':
+        hr=wd[5].split(':')
+        hcount[hr[0]]=hcount.get(hr[0],0)+1
+    else:
+        continue
+for k,v in hcount.items():
+    hlist.append((k,v))
+hlist.sort()
+for k,v in hlist:
+    print (k,v)
+
